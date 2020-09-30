406171963fdd1f3b8b0f680ea83ec4581f1b6160
Filipo Perotto <filipo.perotto@litislab.fr>
2020-09-30 10:58:36 +0100

    correction setup

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ setuptools.setup(
     author_email="filipo.perotto.univ@gmail.com",
     description="A compilation of text tiling, text segmentation and text classification methods.",
     long_description=long_description,
+	#long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/fsperotto/lts",
-    packages=setuptools.find_packages(),
+    #packages=setuptools.find_packages(),
+	packages=['lts', 'lts.emb_seg_text'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
