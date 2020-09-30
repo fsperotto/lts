48dfbde371de85e7eea30493c4585ef4caa7a35a
Filipo Perotto <filipo.perotto@litislab.fr>
2020-09-30 10:35:12 +0100

    Include emb_text_seg method.

---
+++ lts/__init__.py
@@ -0,0 +1,1 @@
+from emb_text_seg import emb_text_seg
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lts-pkg-fsperotto", # Replace with your own username
+    version="0.0.1",
+    author="Filipo Studzinski Perotto",
+    author_email="filipo.perotto.univ@gmail.com",
+    description="A compilation of text tiling, text segmentation and text classification methods.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fsperotto/lts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
