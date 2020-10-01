e5557bf6274d635ef29d5b69378f3e86fec077dd
Filipo Studzinski Perotto <fstudzin@irit.fr>
2020-10-01 16:50:38 +0200

    Signed-off-by: Filipo Studzinski Perotto <fstudzin@irit.fr>

--- lts/slts_corpus.py
+++ lts/slts_corpus.py
@@ -1,3 +1,9 @@
+
+import string   # text strings manipulation
+import os       # operating system and file operations
+import fnmatch  # string search 
+import re       # regular expressions
+
 class slts_corpus:
 
     def __init__(self, labels, name="corpus", url=None):  
