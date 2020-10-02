16427c116b828383fef9308947e1c54a971edd03
Filipo Studzinski Perotto <fstudzin@irit.fr>
2020-10-02 13:17:00 +0200

    Signed-off-by: Filipo Studzinski Perotto <fstudzin@irit.fr>

--- lts/__init__.py
+++ lts/__init__.py
@@ -1,5 +1,3 @@
-#from importlib import import_module
-#
 # **pkgutil-style namespace packages**
 #
 #Python 2.3 introduced the pkgutil module and the extend_path function. 
@@ -16,15 +14,15 @@
 #          module.py
 #
 #The __init__.py file for the namespace package needs to contain only the following:
-#
-#__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-#__path__ = ['.', '.lts']
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 #
 #
-#
+
 __version__ = (0, 0, 2)
-#
+
+
 #
 # "import lts" will import the following classes but also the corresponding modules (.py files)
 #
@@ -35,20 +33,12 @@ names=[
             {'classname':'C99',                     'basemodulename':'uts',          'submodulename':'.c99'},
             {'classname':'TextTiling',              'basemodulename':'uts',          'submodulename':'.texttiling'}
         ]
-#
+
 for name in names:
     exec(f'from .{name["basemodulename"]}{name["submodulename"]} import {name["classname"]}')
-#from .emb_text_seg import EmbeddingsTextSegmenter
-#from .slts_corpus import SegmentedCorpus
-#from .pre_proc import TextPreProcessor
-#from .uts.c99 import C99
-#from .uts.texttiling import TextTiling
+
 #
 # for import *:
 #
-#__all__ = ["uts", "texttiling", "emb_text_seg", "slts_corpus", "pre_proc", "SegmentedCorpus", "EmbeddingsTextSegmenter", "TextPreProcessor", "C99"]
 __all__ = list(set([name['basemodulename'] for name in names] + [name['classname'] for name in names]))
 ["uts", "texttiling", "emb_text_seg", "slts_corpus", "pre_proc", "SegmentedCorpus", "EmbeddingsTextSegmenter", "TextPreProcessor", "C99"]
-
-#from .fad_loader import *
-#from .uts import *
